b43e965f54433f0534f0d7e5ec86d5f0dfb5ba93
Sarah Brouillet <sarah.brouillet@gmail.com>
2020-12-18 18:52:33 -0500

    Version responsive de la page index.html complétée, variables de padding et margin ajoutées

--- src/js/script.js
+++ src/js/script.js
@@ -8,23 +8,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
         //console.log(params);
 
-        if(params.get("id") === null){
-            connexion.requeteFilmRecent();
-        }
-        else {
-            connexion.requeteInfoFilm(params.get("id"));
-        }
-
-    }
-    else {
+        connexion.requeteInfoFilm(params.get("id"));
+    } else {
         connexion.requeteDernierFilm();
         connexion.requeteFilmsPopulaires();
     }
 
-
+    //MENU MOBILE
     var hamburger = document.querySelector(".hamburger");
     var menuMobile = document.querySelector(".nav-mobile");
-    console.log(hamburger);
+    //console.log(hamburger);
     hamburger.addEventListener("click", openMenu);
 
     function openMenu(evt){
@@ -42,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-
+    //BOUTON RETOUR EN HAUT DE LA PAGE
     document.querySelector(".material-icons").addEventListener("click", fScrollToTop);
 
     function fScrollToTop(e){
@@ -51,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function () {
             left: 0,
             behavior: 'smooth'
         })
-    }/**/
+    }
 });
 
 
@@ -115,7 +108,7 @@ class MovieDB {
 
             unArticle.querySelector("h3").innerHTML = data[i].title;
             unArticle.querySelector("p.description").innerHTML = data[i].overview || "Description non disponible";
-            unArticle.querySelector("p.cote").innerHTML = data[i].vote_average;
+            unArticle.querySelector("p.cote").innerHTML = data[i].vote_average + "/10";
             unArticle.querySelector("p.annee").innerHTML = data[i].release_date;
 
             let src = this.imgPath + "w185" + data[i].poster_path;
@@ -171,7 +164,7 @@ class MovieDB {
 
             //console.log(unArticle.querySelector("h2"));
             unArticle.querySelector("h3").innerHTML = data[i].title;
-            unArticle.querySelector("p.cote").innerHTML = data[i].vote_average;
+            unArticle.querySelector("p.cote").innerHTML = data[i].vote_average + "/10";
 
             let src = this.imgPath + "w185" + data[i].poster_path;
 
@@ -226,7 +219,7 @@ class MovieDB {
         document.querySelector("h1").innerHTML = data.title;
         document.querySelector("p.description").innerHTML = data.overview || "Description non disponible";
         document.querySelector("p.annee").innerHTML = "Année de parution : " + (data.release_date || "Non disponible");
-        document.querySelector("p.vote").innerHTML = "Note moyenne : " + data.vote_average || "Non disponible";
+        document.querySelector("p.vote").innerHTML = "Note moyenne : " + data.vote_average + "/10" || "Non disponible";
 
         if(data.original_language === "en"){
             document.querySelector("p.lang").innerHTML = "Langue originale : Anglais";
@@ -273,81 +266,6 @@ class MovieDB {
 
         unArticle.querySelector("a").setAttribute("href", "fiche-film.html?id=" + data[i].id);*/
 
-    }
-
-
-
-    requeteFilmRecent() {
-        //Mettre une page web à jour sans perturber les actions de l'utilisateur
-        let requeteXhr = new XMLHttpRequest();
-
-        requeteXhr.addEventListener("loadend", this.retourRequeteFilmRecent.bind(this));
-
-        // https://api.themoviedb.org/3/movie/upcoming?api_key=195be83ab1367b9fb7eed4908ea17b46&language=fr-CA&page=1
-
-        //Initialiser la requête pour récupérer les films
-        requeteXhr.open("GET", this.baseURL + "/movie/upcoming?api_key=" + this.APIKey + "&language=" + this.lang + "&page=1");
-
-        //Envoyer la requête
-        requeteXhr.send();
-    }
-
-    retourRequeteFilmRecent(e) {
-        let target = e.currentTarget;
-
-        //console.log(target.responseText);
-
-        let data;
-
-        data = JSON.parse(target.responseText).results;
-
-        console.log(data);
-
-        this.afficheFilmRecent(data);
-    }
-
-    afficheFilmRecent(data) {
-        for (let i = 0; i < 1; i++){
-            //console.log(data[0]);
-            document.querySelector("h1").innerHTML = data[0].title;
-            document.querySelector("p.description").innerHTML = data[0].overview || "Description non disponible";
-            document.querySelector("p.annee").innerHTML = "Année de parution : " + (data[0].release_date || "Non disponible");
-            document.querySelector("p.vote").innerHTML = "Note moyenne : " + data[0].vote_average || "Non disponible";
-
-            if(data[0].original_language === "en"){
-                document.querySelector("p.lang").innerHTML = "Langue originale : Anglais";
-            } else if(data[0].original_language === "fr"){
-                document.querySelector("p.lang").innerHTML = "Langue originale : Français";
-            } else{
-                document.querySelector("p.lang").innerHTML = "Langue originale : " + (data[0].original_language || "Non disponible");
-            }
-
-            if(data[0].runtime === 0 || data[0].runtime === undefined){
-                document.querySelector("p.duree").innerHTML = "Durée : Non disponible";
-            } else{
-                document.querySelector("p.duree").innerHTML = "Durée : " + data[0].runtime + " minutes";
-            }
-
-            if(data[0].budget === 0 || data[0].budget === undefined){
-                document.querySelector("p.budget").innerHTML = "Budget : Non disponible";
-            } else{
-                document.querySelector("p.budget").innerHTML = "Budget : " + data[0].budget + "$";
-            }
-
-            if(data[0].revenue === 0 || data[0].revenue === undefined){
-                document.querySelector("p.recette").innerHTML = "Revenus : Non disponible";
-            } else{
-                document.querySelector("p.recette").innerHTML = "Revenus : " + data[0].revenue + "$";
-            }
-
-            document.querySelector("p.description").innerHTML = data[0].overview || "Description non disponible";
-
-            let src = this.imgPath + "w185" + data[0].backdrop_path;
-            document.querySelector("img.affiche").setAttribute("src", src);
-            document.querySelector("img.affiche").setAttribute("alt", data[0].title);
-
-            this.requeteActeur(data[0].id);
-        }
     }
 
 
